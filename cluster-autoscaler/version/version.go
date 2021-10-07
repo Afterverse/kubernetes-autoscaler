9b02167dd68b4f4ae88518e91b98bd2a487ff086
José Carlos Cieni Júnior <cienijr@users.noreply.github.com>
2021-10-07 14:41:59 -0300

    Bump version

--- cluster-autoscaler/version/version.go
+++ cluster-autoscaler/version/version.go
@@ -17,4 +17,4 @@ limitations under the License.
 package version
 
 // ClusterAutoscalerVersion contains version of CA.
-const ClusterAutoscalerVersion = "1.20.0-PLAYKIDS-1"
+const ClusterAutoscalerVersion = "1.20.0-PLAYKIDS-2"
